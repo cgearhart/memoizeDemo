f64803e4f61d3f0d889a1ed960fba2a3c48d0134
CGearhart <cgearhart@gmail.com>
2012-08-08 09:43:01 -0500

    Greatly simplified example. Demonstrates a single list length and changed to a more recursive-intense function (fibonacci from factorial).

--- memoizeDemo.py
+++ memoizeDemo.py
@@ -16,41 +16,34 @@ def print_timing(func):
 def loop_over(func,list):
     return map(func,list)
 
+
 ########################### Memoize Function ###################################
 
 def memoize(function):
-    # Create and return a memoized version of a function
     value_cache = {}
     def memoizedFunction(*args):
         if args not in value_cache:
             value_cache[args] = function(*args)
         return value_cache[args]
-    return memoizedFunction # this returns a function reference - not a value
-
+    return memoizedFunction
+            
 
 ########################### Main ###############################################
 
 if __name__ == '__main__':
-    for list_length in range(10,100,10):
-        
-        # redefine the functions every time the loop runs to avoid time savings
-        # in memoize from previous runs
-        def fact(n):
-            return 1 if n in [0,1] else n * fact(n-1)
 
-        @memoize
-        def memfact(n):
-            return 1 if n in [0,1] else n * memfact(n-1)
-        
-        # generate random sequence of ints; shows the avg effect of memoization
-        randSequence = range(list_length)
-        random.shuffle(randSequence)
+    def fib(n):
+        return n if n in [0,1] else fib(n-1) + fib(n-2)
 
-        print ('For the sequence: {}, containing {} elements.'
-               .format(randSequence, len(randSequence))
-               ) # show the sequence and the number of elements
-        print 'Normal factorial took:'
-        loop_over(fact,randSequence) # run fact on every element
-        print 'Memoized factorial took:'
-        loop_over(memfact,randSequence) # run memfact on every element
-        print ''
+    @memoize
+    def memfib(n):
+        return n if n in [0,1] else memfib(n-1) + memfib(n-2)
+    
+    sequence = range(30) # Warning! range(40) takes several minutes to run
+    print 'For {} elements:'.format(len(sequence))
+    print 'Normal fib took:'
+    loop_over(fib,sequence) # approx average 2080 ms for 30 elements
+    print 'Memoized fib took:'
+    loop_over(memfib,sequence) # approx average .126 ms for 30 elements
+    print ''
+
